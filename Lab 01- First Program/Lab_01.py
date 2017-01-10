46b7f55daef9f96d9bca472f99cacfb1496dead6
SC\daniel.camacho <daniel.camacho@my.simpson.edu>
2017-01-10 17:10:58 -0600

    Making the Lab mine

--- Lab 01- First Program/Lab_01.py
+++ Lab 01- First Program/Lab_01.py
@@ -1,3 +1,12 @@
-print(" It was a dark and stormy night.")
-print("Suddenly a shot rang out!")
-print("foot of her bed and watched the trees tossing in the frenzied lashing of the wind2.")
+print('My favorite movie is "Hachi: A Dog\'s Tale".')
+print("Thanks to the movie, I relized that dogs are very helpful and can be used for many tasks.")
+print()
+print("There are service dogs that assist blind people and other dogs that predict when the owner is vulnerable to a possible heart attack!\nJust like Hachi did in the movie!")
+print()
+print("Dogs can reveal many secrets and bring many benefits to our life: we just have to pay attention to them and \ntreat them like a good friend.")
+
+
+
+
+
+
