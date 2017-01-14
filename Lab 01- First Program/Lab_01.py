a3835e42cad16d35b13bf8ad00e1f109b59ff766
DESKTOP-76P8UFS\Daniel Camacho <daniel.camacho@my.simpson.edu>
2017-01-13 22:04:42 -0600

    Correcting errors in first lab

--- Lab 01- First Program/Lab_01.py
+++ Lab 01- First Program/Lab_01.py
@@ -1,9 +1,9 @@
 print('My favorite movie is "Hachi: A Dog\'s Tale".')
-print("Thanks to the movie, I relized that dogs are very helpful and can be used for many tasks.")
+print("Thanks to the movie, I realized that dogs are very helpful and can be used for many tasks.")
 print()
 print("There are service dogs that assist blind people and other dogs that predict when the owner is vulnerable to a possible heart attack!\nJust like Hachi did in the movie!")
 print()
-print("Dogs can reveal many secrets and bring many benefits to our life: we just have to pay attention to them and \ntreat them like a good friend.")
+print("Dogs can reveal many secrets and be of great company. They bring many benefits to our life: we just have to pay attention to them and \ntreat them like a good friend.")
 
 
 
