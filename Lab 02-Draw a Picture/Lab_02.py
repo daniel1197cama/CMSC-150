3b618986f4b65f9756bd29ce8a9b45fcd8e7813b
SC\daniel.camacho <daniel.camacho@my.simpson.edu>
2017-01-17 15:41:06 -0600

    Spacecraft Spacex

--- Lab 02-Draw a Picture/Lab_02.py
+++ Lab 02-Draw a Picture/Lab_02.py
@@ -2,10 +2,19 @@ import arcade
 arcade.open_window("Hubble Space Telescope", 600,600)
 
 #Remember to change background color to space black.
-arcade.set_background_color((75,166,196))
+arcade.set_background_color((1,8,8))
 arcade.start_render()
-arcade.draw_circle_filled(250,300,50,(165,170,173))
-arcade.draw_circle_outline(250,300,50,(126,130,133))
+arcade.draw_lrtb_rectangle_filled(190,400,400,250,(203,214,214))
+arcade.draw_lrtb_rectangle_filled(180,190,300,290,(203,214,214))
+arcade.draw_lrtb_rectangle_filled(400,410,300,290,(203,214,214))
+
+#Solar panels
+arcade.draw_lrtb_rectangle_filled(00,190,300,290,(203,214,214))
+
+
+
+arcade.draw_circle_filled(250,100,50,(165,170,173))
+arcade.draw_circle_outline(250,100,50,(126,130,133))
 
 
 
