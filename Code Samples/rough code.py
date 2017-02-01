3e7d4d98aa4df966028db9f8f7e14b434e64c864
SC\daniel.camacho <daniel.camacho@my.simpson.edu>
2017-02-01 08:59:48 -0600

    probability

--- Code Samples/rough code.py
+++ Code Samples/rough code.py
@@ -1,4 +1,19 @@
-i = 10
-while i < 10:
-    print(i)
-    i -= 1
+import random
+
+head_count = 0
+tail_count = 0
+
+for i in range(50):
+    random_number = random.randrange(2)
+
+    if random_number == 0:
+        print("heads")
+        head_count += 1
+    else:
+        print("tails")
+        tail_count += 1
+
+print("Heads", head_count)
+print("Tails", tail_count)
+
+
