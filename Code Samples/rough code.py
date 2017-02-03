95617a59bd1e1920bf4ed99ef46e5e92f911112d
SC\daniel.camacho <daniel.camacho@my.simpson.edu>
2017-02-03 08:58:42 -0600

    hola

--- Code Samples/rough code.py
+++ Code Samples/rough code.py
@@ -16,4 +16,31 @@ for i in range(50):
 print("Heads", head_count)
 print("Tails", tail_count)
 
+for row in range(10):
+    for column in range(row):
+        print(".", end=" ")
+    for column in range(10 - row):
+            print(column, end=" ")
+    print()
+
+print()
+
+for row in range(1, 10):
+    for column in range(9 - row, 0, -1):
+        print("*", end=" ")
+    for column in range(1, row + 1):
+        print(column, end=" ")
+
+    for column in range(row - 1, 0, -1):\
+        print(column, end=" ")
+
+    print()
+
+print()
+
+for i in range(20):
+    if i % 3 == 0:
+        print("Fizz")
+    else:
+        print(i)
 
