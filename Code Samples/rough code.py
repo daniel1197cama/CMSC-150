32639cf6cf264ba2d5dbf8fea9e648d944b891fa
SC\daniel.camacho <daniel.camacho@my.simpson.edu>
2017-02-10 08:59:17 -0600

    Class Notes

--- Code Samples/rough code.py
+++ Code Samples/rough code.py
@@ -22,7 +22,7 @@ print(smallest_number)
 
 # ord() function, letter to a number
 # chr() function, number to a letter
-
+'''
 plain_text = "This is a test. ABCDEF"
 for character in plain_text:
     x = ord(character)
@@ -35,3 +35,77 @@ for character in encrypted_text:
     x = ord(character)
     x -=2
     print(chr(x), end=" ")
+'''
+"""
+#0
+room = [ 1, None, None, None]
+room_list.append(room)
+
+#1
+room = [ 2, None, None, None]
+room_list.append(room)
+
+#2
+room = [ 3, None, None, None]
+room_list.append(room)
+
+#3
+room = [None, None, 2, None]
+room_list.append(room)
+
+#4
+room = [None, None, None, 2]
+room_list.append(room)
+"""
+'''
+name = "Link"
+sex = "Male"
+max_hit_points = 50
+current_hit_points = 50
+max_speed = 10
+
+def d
+'''
+"""
+class Character():
+    ### This is a class that represents the main character in a game ###
+    def __init__(self): # init stands for initialized
+        self.name = "Link"
+        self.sex = "Male"
+        self.max_hit_points = 50
+        self.max_hit_points = 50
+        self.current_hit_points = 50
+        self.max_speed = 10
+
+class Adress():
+    def __init__(self):
+        self.name = ""
+        self.line1 = ""
+        self.line2 = ""
+        self.city = ""
+        self.state = ""
+        self.zip = ""
+
+home_address = Adress()
+home_address.name = "John Smith"
+home_address.line1 = " 701 N. C Street"
+home_address.line2 = "Carver Science Building"
+home_address.city =  "Indianola"
+home_address.state = "IA"
+home_address.zip = "50125"
+"""
+class Dog():
+    def __init__(self):
+        self.age = 0
+        self.name = ""
+        self.weight = 0
+
+    def bark(self):
+        print("woof says " + self.name + ".")
+
+dog_information = Dog()
+dog_information.name = 2
+dog_information.age = "Willi"
+dog_information.weight = 15
+
+dog_information.bark()
